67af67f1e8cf1d24ed1d55ef1c5fd9ffa5fd7a91
adamclear <3589@holbertonschool.com>
2023-02-21 19:33:13 +0000

    file for task 3

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'dart:async';
+
+FutureOr<String> greetUser() async {
+	return fetchUserData().then((value) => 'hello ${json.decode(value)['username']}')
+	.catchError((error) => "error caught: $error");
+}
+
+Future<String> loginUser() async {
+	return checkCredentials().then((value) => value ? greetUser() : 'Wrong credentials');
+}
+
+Future<String> fetchUserData() => Future.delayed(
+      const Duration(seconds: 2),
+      () =>
+          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
+    );
+
+Future<bool> checkCredentials() =>
+    Future.delayed(const Duration(seconds: 2), () => true);
