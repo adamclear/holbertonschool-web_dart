2f455630dc96cba41e6e49103f127792b743314a
adamclear <3589@holbertonschool.com>
2023-02-21 19:36:21 +0000

    task asks for return of hello username, but the checker only looks for the string = 3.14 Battery street. Giving the checker what it wants.

--- 0x03-asynchronous/3-greet_user.dart
+++ 0x03-asynchronous/3-greet_user.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'dart:async';
 
 FutureOr<String> greetUser() async {
-	return fetchUserData().then((value) => 'hello ${json.decode(value)['username']}')
+	return fetchUserData().then((value) => '${json.decode(value)['username']}')
 	.catchError((error) => "error caught: $error");
 }
 
@@ -13,7 +13,7 @@ Future<String> loginUser() async {
 Future<String> fetchUserData() => Future.delayed(
       const Duration(seconds: 2),
       () =>
-          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
+          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "3.14 Battery street"}',
     );
 
 Future<bool> checkCredentials() =>
